be891c2fd2a2481eb3c88adf89dbc13309b46b0e
AliphaticGuru <sirajmuhd2@gmail.com>
2025-12-22 09:12:57 +0100

    first Laravel bootcamp app commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+
+FROM hardcore28/lara-base:8.4 AS app
+
+WORKDIR /var/www/html
+
+
---
+++ src/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
