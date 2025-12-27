426a4e7682426f5ab2c1ff387dddd40122907a3f
AliphaticGuru <sirajmuhd2@gmail.com>
2025-12-27 20:48:53 +0100

    new component in src, and many more

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,19 +1,15 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [ChirpController::class, 'index']);
 
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/home', [ChirpController::class, 'index']);
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', [ChirpController::class, 'about']);
