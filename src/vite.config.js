3d9954b77cd9fb929829b52bc717a30f3d68db1b
AliphaticGuru <sirajmuhd2@gmail.com>
2025-12-28 22:34:30 +0100

    vite config 01

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,32 +3,31 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0', 
-        // origin: 'http://host.docker.internal:5173',
-        strictPort: true,
-        port: 5173,
-        hmr: {
-            host: process.env.VITE_DEV_SERVER_HOST || 'localhost',
-            protocol: 'ws',
-            port: 5173,
-        },
-        // watch: {
-        //     usePolling: true,
-        // }
-    },
+    // server: {
+    //     host: '0.0.0.0', 
+    //     strictPort: true,
+    //     port: 5173,
+    //     hmr: {
+    //         host: process.env.VITE_DEV_SERVER_HOST || 'localhost',
+    //         protocol: 'ws',
+    //         port: 5173,
+    //     },
+    // },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                    'resources/css/app.css', 
+                    'resources/js/app.js'
+                ],
             refresh: true,
         }),
         tailwindcss()
     ],
-    build: {
-        manifest: true,
-        outDir: 'public/build',
-        rollupOptions: {
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-        },
-    },
+    // build: {
+    //     manifest: true,
+    //     outDir: 'public/build',
+    //     rollupOptions: {
+    //         input: ['resources/css/app.css', 'resources/js/app.js'],
+    //     },
+    // },
 });
